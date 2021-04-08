836d8e45f28660b35d77e5725a43d6ddfcd3c1a8
klenkiven <wzl709@outlook.com>
2021-04-08 18:14:45 +0800

    (feat)医院设置--功能模块搭建

---
+++ service/service_hospital/src/main/java/xyz/klenkiven/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package xyz.klenkiven.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：klenkiven
+ * @date ：2021/4/8 18:12
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class);
+    }
+
+}
