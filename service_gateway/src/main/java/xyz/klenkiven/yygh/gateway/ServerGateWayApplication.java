205d67e153c4e1b90b13af8f8a8afca2aa1f5a40
klenkiven <wzl709@outlook.com>
2021-05-06 21:36:11 +0800

    (fix) alibaba-cloud版本问题解决gateway

--- service_gateway/src/main/java/xyz/klenkiven/yygh/gateway/ServerGateWayApplication.java
+++ service_gateway/src/main/java/xyz/klenkiven/yygh/gateway/ServerGateWayApplication.java
@@ -2,8 +2,10 @@ package xyz.klenkiven.yygh.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServerGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerGateWayApplication.class, args);
